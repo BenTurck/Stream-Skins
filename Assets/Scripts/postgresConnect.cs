49406c3bd63ecb1bb5b36d998898a06d1f50f8ec
Ben Turck <benturck@outlook.com>
2021-04-08 15:45:09 -0400

    postgresConnect

---
+++ Assets/Scripts/postgresConnect.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace postgresConnect
+{
+
+    public class Registration : MonoBehaviour
+    {
+
+        public InputField nameField;
+        public InputField passwordField;
+
+        public Button submitButton;
+
+        public void callRegister()
+        {
+
+            StartCoroutine(Register());
+        }
+
+        IEnumerator Register()
+        {
+
+            WWWForm form = new WWWForm();
+            form.AddField("name", nameField.txt);
+            form.AddField("password", passwordField.txt);
+
+            WWW www = new WWW(http://localhost/sqlconnect/register.php);
+            yield return www;
+
+            if (www.text == "0")
+            {
+                Debug.Log("User created successfully. ");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Debug.Log("Failed to create user" + www.txt);
+            }
+        }
+
+        public void VerifyInput()
+        {
+            submitButton.interactable = (nameField.txt.length >= 8 && passwordField.text.Length >= 8);
+        }
+    }
+}
