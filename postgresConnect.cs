5038ace4bd4afcb66e4d9da794ed2b22cc8d7007
Christian Lindsey <44047098+christuhfur@users.noreply.github.com>
2021-04-08 14:57:33 -0400

    postgresConnect.cs
    (Register Account Related Buttons)
    
    NameInputField and PasswordField can be dragged into their respectable places
    
    UserNameButton = NameInputField (InputField)
    PassWordButton = PasswordInputField (InputField)

---
+++ postgresConnect.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace postgresConnect {
+
+    public class Registration : Monobehaviour {
+
+        public InputField nameField;
+        public InputField passwordField;
+
+        public Button submitButton;
+
+        public void callRegister() {
+
+            startCoroutine(Register())
+        }
+
+        IEnumerator Register() {
+
+            WWWForm form = new WWWForm();
+            form.AddField("name", nameField.txt);
+            form.Addfield("password", passwordField.txt);
+
+            WWW www = new WWW(http://localhost/sqlconnect/register.php);
+            yield return www;
+
+            if(www.text == "0") {
+                Debug.Log("User created successfully. ");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+            else {
+                Debug.log("Failed to create user" + www.txt);
+            }
+        }
+
+        public void VerifyInput() {
+            submitButton.interactable = (nameField.txt.length >= 8 && passwordField.text.length >= 8);
+        }
+    }
+}
