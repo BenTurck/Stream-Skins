a5b8238fd497bd6e7ca1e9be747fad618e849e9d
Ben Turck <benturck@outlook.com>
2021-04-25 15:48:44 -0400

    Valorant Scene
    Added functionality to the home team buttons and input fields.

--- Assets/Scripts/Valorant Scripts/ValorantWritetoFile.cs
+++ Assets/Scripts/Valorant Scripts/ValorantWritetoFile.cs
@@ -9,20 +9,38 @@ using System.IO;
 public class ValorantWritetoFile : MonoBehaviour
 {
 	public Dropdown ValorantHomeDDInput;
+
 	private string ValorantHomeStringInput;
 
+	public string ValorantHomeSeriesScoreInput;
+
+	public InputField ValorantHomeSeriesScoreInputField;
+
+	public int ValorantHomeSeriesScoreValue = 0;
+
 	public Dropdown ValorantAwayDDInput;
+
 	private string ValorantAwayStringInput;
-	 //Start is called before the first frame update
+
+
+
+
+	//Start is called before the first frame update
 	void Start()
-    {
+	{
 		Directory.CreateDirectory("Assets/Games/Valorant/");
 		Directory.CreateDirectory("Assets/TeamLogos/");
+
+
+
+
+
 		ValorantHomeDDInput.onValueChanged.AddListener(delegate
 		{
 			ValorantHomeDDValue(ValorantHomeDDInput);
 
 		});
+
 		ValorantAwayDDInput.onValueChanged.AddListener(delegate
 		{
 			ValorantAwayDDValue(ValorantAwayDDInput);
@@ -30,16 +48,17 @@ public class ValorantWritetoFile : MonoBehaviour
 		});
 
 
+
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
 
-    public void ValorantHomeDDValue(Dropdown sender)
-    {
+	}
+
+	public void ValorantHomeDDValue(Dropdown sender)
+	{
 		ValorantHomeStringInput = ValorantHomeDDInput.options[ValorantHomeDDInput.value].text;
 		Debug.Log("Home Team Name.txt: " + ValorantHomeStringInput);
 		//Directory.CreateDirectory("Assets/Games/Smash Bros/"); // creates a directory differently in this path
@@ -95,6 +114,161 @@ public class ValorantWritetoFile : MonoBehaviour
 			File.WriteAllText(path, ValorantHomeStringInput); // sets the location to path and the contents to input
 		}
 	}
+
+	public void ValorantHomeSeasonRecord(string s)
+	{
+		string ValorantHomeSeasonRecordInput = s;
+
+		Debug.Log("Home Team Season Record.txt: " + ValorantHomeSeasonRecordInput); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Season Record.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomeSeasonRecordInput); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomeSeasonRecordInput);
+	}
+
+	public void ValorantHomeSeriesScoreIncrement()
+	{
+		string maxValue = "3";
+		string path = "Assets/Games/Valorant/Home Team Series Record.txt";
+		if (ValorantHomeSeriesScoreInputField != null && ValorantHomeSeriesScoreInputField.text != maxValue)
+		{
+			++ValorantHomeSeriesScoreValue;
+			ValorantHomeSeriesScoreInput = ValorantHomeSeriesScoreValue.ToString();
+			ValorantHomeSeriesScoreInputField.text = ValorantHomeSeriesScoreValue.ToString();
+			Debug.Log("Increment - Home Team Series Record.txt: " + ValorantHomeSeriesScoreInputField.text);
+			File.WriteAllText(path, ValorantHomeSeriesScoreInputField.text);
+		}
+	}
+	public void ValorantHomeSeriesScoreDecrement()
+	{
+		string minValue = "0";
+		string path = "Assets/Games/Valorant/Home Team Series Record.txt";
+		if (ValorantHomeSeriesScoreInputField != null && ValorantHomeSeriesScoreInputField.text != minValue)
+		{
+			--ValorantHomeSeriesScoreValue;
+			ValorantHomeSeriesScoreInput = ValorantHomeSeriesScoreValue.ToString();
+			ValorantHomeSeriesScoreInputField.text = ValorantHomeSeriesScoreValue.ToString();
+			Debug.Log("Decrement - Home Team Series Record.txt: " + ValorantHomeSeriesScoreInputField.text);
+			File.WriteAllText(path, ValorantHomeSeriesScoreInputField.text);
+		}
+	}
+
+	public void ValorantHomeSeriesScore(string s)
+	{
+		string path = "Assets/Games/Valorant/Home Team Series Record.txt";
+		ValorantHomeSeriesScoreInput = s;
+		ValorantHomeSeriesScoreInput = ValorantHomeSeriesScoreValue.ToString();
+		ValorantHomeSeriesScoreValue = Convert.ToInt32(ValorantHomeSeriesScoreInputField.text);
+		Debug.Log("Input - Home Team Series Record.txt: " + ValorantHomeSeriesScoreValue);
+		File.WriteAllText(path, ValorantHomeSeriesScoreInputField.text);
+	}
+
+	public void ValorantHomePlayer1(string s)
+	{
+		string ValorantHomePlayer1Input = s;
+
+		Debug.Log("Home Team Player1.txt: " + ValorantHomePlayer1Input); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Player1.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomePlayer1Input); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomePlayer1Input);
+	}
+
+	public void ValorantHomePlayer2(string s)
+	{
+		string ValorantHomePlayer2Input = s;
+
+		Debug.Log("Home Team Player2.txt: " + ValorantHomePlayer2Input); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Player2.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomePlayer2Input); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomePlayer2Input);
+	}
+
+	public void ValorantHomePlayer3(string s)
+	{
+		string ValorantHomePlayer3Input = s;
+
+		Debug.Log("Home Team Player3.txt: " + ValorantHomePlayer3Input); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Player3.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomePlayer3Input); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomePlayer3Input);
+	}
+
+	public void ValorantHomePlayer4(string s)
+	{
+		string ValorantHomePlayer4Input = s;
+
+		Debug.Log("Home Team Player4.txt: " + ValorantHomePlayer4Input); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Player4.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomePlayer4Input); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomePlayer4Input);
+	}
+
+	public void ValorantHomePlayer5(string s)
+	{
+		string ValorantHomePlayer5Input = s;
+
+		Debug.Log("Home Team Player5.txt: " + ValorantHomePlayer5Input); // shows input in debug
+
+		Directory.CreateDirectory("Assets/Games/Valorant/"); // creates directory in this path
+															 // Application.streamingAssetsPath sets the directory to be in the data folder and creates folders called
+															 // streamingAssetsPath, LIVE_DATA, and Smash Bros
+
+		string path = "Assets/Games/Valorant/Home Team Player5.txt"; // This is the directory for the text file.
+		if (!File.Exists(path)) // checks if the file exists at the path
+		{
+			File.WriteAllText(path, ValorantHomePlayer5Input); // sets the location to path and the contents to input inside if statement
+		}
+
+		File.WriteAllText(path, ValorantHomePlayer5Input);
+	}
+
+
+
+
 
 	public void ValorantAwayDDValue(Dropdown sender)
 	{
